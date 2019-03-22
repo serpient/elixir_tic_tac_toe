2a12bc97aebfb944e99868512a6c58b87beef109
serpient <littlegangwolf@gmail.com>
2019-03-22 11:15:06 -0700

    test to print board to CL pass

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -1,6 +1,12 @@
 defmodule TTT do
   def get_player_name() do
     IO.gets("What is your name? \n")
-    |> IO.puts
+    |> IO.puts()
+  end
+
+  def print_board() do
+    IO.puts(
+      ~s(1    |2    |3    \n     |     |     \n_____|_____|_____\n4    |5    |6    \n     |     |     \n_____|_____|_____\n7    |8    |9    \n     |     |     \n     |     |     \n)
+    )
   end
 end
--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -6,4 +6,10 @@ defmodule TTTTest do
   test "prints out question for player input" do
     assert capture_io(fn -> TTT.get_player_name() end) == "What is your name? \neof\n"
   end
+
+  test "prints 3x3 board to CL" do
+    assert capture_io(fn -> TTT.print_board() end) == ~s"""
+           1    |2    |3    \n     |     |     \n_____|_____|_____\n4    |5    |6    \n     |     |     \n_____|_____|_____\n7    |8    |9    \n     |     |     \n     |     |     \n
+           """
+  end
 end
