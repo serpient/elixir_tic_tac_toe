5431571ba12c20d3cc1045ad34ba45e5837ee3d0
serpient <littlegangwolf@gmail.com>
2019-03-22 10:52:57 -0700

    remove starter fn and test

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -1,21 +1,4 @@
 defmodule TTT do
-  @moduledoc """
-  Documentation for TTT.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TTT.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def get_player_name() do
     IO.gets("What is your name? \n")
     |> IO.puts
--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -3,10 +3,6 @@ defmodule TTTTest do
   import ExUnit.CaptureIO
   doctest TTT
 
-  test "greets the world" do
-    assert TTT.hello() == :world
-  end
-
   test "prints out question for player input" do
     assert capture_io(fn -> TTT.get_player_name() end) == "What is your name? \neof\n"
   end
