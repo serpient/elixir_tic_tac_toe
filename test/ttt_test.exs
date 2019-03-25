3aaf3148e912633fe9018ecfe137f96997fb83b8
serpient <littlegangwolf@gmail.com>
2019-03-25 14:10:50 -0700

    game will end when no spaces are left

--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -95,6 +95,7 @@ defmodule TTT_Test do
       8 => " ",
       9 => " "
     }
+
     assert ProcessInput.handle_input("1", initial_board, "O") == {:error, :duplicate_input}
   end
 
@@ -110,6 +111,7 @@ defmodule TTT_Test do
       8 => " ",
       9 => " "
     }
+
     assert ProcessInput.is_empty(1, initial_board) == false
   end
 
@@ -125,6 +127,72 @@ defmodule TTT_Test do
       8 => " ",
       9 => " "
     }
+
     assert ProcessInput.is_empty(2, initial_board) == true
+  end
+
+  test "After no number input, prompt player for another input." do
+    initial_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+
+    assert ProcessInput.handle_input("string", initial_board, "X") == {:error, :invalid_input}
+  end
+
+  test "After board is filled, don't allow more input" do
+    initial_board = %{
+      1 => "X",
+      2 => "O",
+      3 => "X",
+      4 => "O",
+      5 => "X",
+      6 => "O",
+      7 => "X",
+      8 => "O",
+      9 => "X"
+    }
+
+    assert capture_io(fn -> TTT.play(initial_board, nil, "O") end) ==
+             GameOutput.get_message(:board_is_filled) <> "\n"
+  end
+
+  test "Checks for empty spots in board." do
+    initial_board = %{
+      1 => "X",
+      2 => "O",
+      3 => "X",
+      4 => "O",
+      5 => "X",
+      6 => "O",
+      7 => "X",
+      8 => "O",
+      9 => "X"
+    }
+
+    assert TTT.check_for_empty_spaces(initial_board) == false
+  end
+
+  test "Returns true if there is still empty spots in board." do
+    initial_board = %{
+      1 => "X",
+      2 => "O",
+      3 => "X",
+      4 => "O",
+      5 => "X",
+      6 => "O",
+      7 => "X",
+      8 => "O",
+      9 => " "
+    }
+
+    assert TTT.check_for_empty_spaces(initial_board) == true
   end
 end
