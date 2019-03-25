1a2b6fb4567e25f3d5d50236b85dac1774c91ced
serpient <littlegangwolf@gmail.com>
2019-03-25 08:02:23 -0700

    rename file; separate fn for message and board print
    - "game_output" is valid for message and board prints

--- lib/game_text.ex
+++
@@ -1,9 +0,0 @@
-defmodule GameText do
-  def get(key) do
-    game_text = %{
-      initial_board: ~s(1    |2    |3    \n     |     |     \n_____|_____|_____\n4    |5    |6    \n     |     |     \n_____|_____|_____\n7    |8    |9    \n     |     |     \n     |     |     \n),
-      initial_player_prompt: ~s(Player X - Select a numbered spot to start the game!\n),
-    }
-    game_text[key]
-  end
-end
