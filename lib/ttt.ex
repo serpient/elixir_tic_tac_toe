7166ff53481864524aaab3c76cfdf96d0bd7defc
serpient <littlegangwolf@gmail.com>
2019-03-25 13:00:41 -0700

    extract input fns; move results into handle_play

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -1,35 +1,49 @@
 defmodule TTT do
   def print_board(board_data) do
     GameOutput.get_board(board_data)
-    |> IO.puts
+    |> IO.puts()
   end
 
-  def update_board(position_to_update, player_symbol, board_data) do
-    Map.replace!(board_data, position_to_update, player_symbol)
-    |> print_board
+  def get_other_player_symbol(player_symbol) when player_symbol == "X" do
+    "O"
   end
 
-  def game_start() do
-    GameOutput.get_message(:initial_player_prompt)
-    |> IO.gets
+  def get_other_player_symbol(player_symbol) when player_symbol == "O" do
+    "X"
   end
 
-  def play() do
-    initial_board = %{
-      1 => "",
-      2 => "",
-      3 => "",
-      4 => "",
-      5 => "",
-      6 => "",
-      7 => "",
-      8 => "",
-      9 => "",
+  def get_player_input(message) do
+    GameOutput.get_message(message)
+    |> IO.gets()
+  end
+
+  def initial_board() do
+    %{
+      1 => " ",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
     }
-    print_board(initial_board)
-    game_start()
-    |> to_string
-    |> String.to_integer
-    |> update_board("X", initial_board)
+  end
+
+  def handle_play(returned_value, board, current_player) do
+    {status, value} = returned_value
+    if (status == :error) do
+      play(board, value, current_player)
+    else
+      play(value, :initial_player_prompt, get_other_player_symbol(current_player))
+    end
+  end
+
+  def play(board \\ initial_board(), prompt \\ :initial_player_prompt, current_player \\ "X") do
+    print_board(board)
+    get_player_input(prompt)
+    |> ProcessInput.handle_input(board, current_player)
+    |> handle_play(board, current_player)
   end
 end
