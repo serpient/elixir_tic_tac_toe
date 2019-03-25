f107183f71c5f0bf28d79f19c67ae9909d185aec
serpient <littlegangwolf@gmail.com>
2019-03-25 13:01:06 -0700

    update test units per refactor

--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -1,10 +1,11 @@
-defmodule TTTTest do
+defmodule TTT_Test do
   use ExUnit.Case
   import ExUnit.CaptureIO
   doctest TTT
   doctest GameOutput
+  doctest ProcessInput
 
-  test "prints 3x3 board to CL" do
+  test "Prints 3x3 board to CL" do
     initial_board = %{
       1 => "",
       2 => "",
@@ -14,38 +15,116 @@ defmodule TTTTest do
       6 => "",
       7 => "",
       8 => "",
-      9 => "",
+      9 => ""
     }
-    assert capture_io(fn -> TTT.print_board(initial_board) end) == GameOutput.get_board(initial_board) <> "\n"
+
+    assert capture_io(fn -> TTT.print_board(initial_board) end) ==
+             GameOutput.get_board(initial_board) <> "\n"
   end
 
-  test "CL outputs starter text for game" do
-    assert capture_io(fn -> TTT.game_start() end) == GameOutput.get_message(:initial_player_prompt)
-  end
+  # test "CL outputs starter text for game" do
+  #   assert capture_io(fn -> TTT.get_player_input(:initial_player_prompt) end) ==
+  #            GameOutput.get_message(:initial_player_prompt)
+  # end
 
   test "After Player X input, board is correctly displayed with new input" do
     initial_board = %{
-      1 => "",
-      2 => "",
-      3 => "",
-      4 => "",
-      5 => "",
-      6 => "",
-      7 => "",
-      8 => "",
-      9 => "",
+      1 => " ",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
     }
+
     updated_board = %{
       1 => "X",
-      2 => "",
-      3 => "",
-      4 => "",
-      5 => "",
-      6 => "",
-      7 => "",
-      8 => "",
-      9 => "",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
     }
-    assert capture_io(fn -> TTT.update_board(1, "X", initial_board) end) == GameOutput.get_board(updated_board) <> "\n"
+
+    assert ProcessInput.handle_input("1", initial_board, "X") == {:ok, updated_board}
+  end
+
+  test "After Player O input, board is correctly displayed with new input" do
+    initial_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+
+    updated_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => "O",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+
+    assert ProcessInput.handle_input("4", initial_board, "O") == {:ok, updated_board}
+  end
+
+  test "After overlapping input, prompt player for another input." do
+    initial_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+    assert ProcessInput.handle_input("1", initial_board, "O") == %{:error => :invalid_input}
+  end
+
+  test "IsEmpty fn returns false if position has player symbol." do
+    initial_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+    assert ProcessInput.is_empty(1, initial_board) == false
+  end
+
+  test "IsEmpty fn returns true if position has no player symbol." do
+    initial_board = %{
+      1 => "X",
+      2 => " ",
+      3 => " ",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+    assert ProcessInput.is_empty(2, initial_board) == true
   end
 end
