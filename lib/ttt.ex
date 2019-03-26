0dac1bbe4422691c0c75c315e6fec2c32320449c
serpient <littlegangwolf@gmail.com>
2019-03-26 11:14:50 -0700

    refactor play handling

--- lib/ttt.ex
+++ lib/ttt.ex
@@ -44,9 +44,8 @@ defmodule TTT do
     {_status, updated_board} = returned_value
 
     print_board(updated_board)
-
-    "Player " <> current_player <> GameOutput.get_message(:wins_game)
-    |> IO.puts()
+    "Player #{current_player} - " <> GameOutput.get_message(:wins_game)
+    |> IO.puts
   end
 
   def handle_error(error, board, current_player) do
@@ -58,6 +57,23 @@ defmodule TTT do
     end
   end
 
+  def handle_validation_result(input_validation_result, board) do
+    {status, _result} = input_validation_result
+    cond do
+      status == :error -> input_validation_result
+      status == :ok -> CheckForWins.analyze(board)
+    end
+  end
+
+  def handle_win_check_result(win_result, input_validation_result, current_player, board) do
+    {win_result_status, win_result_code} = win_result
+        cond do
+          win_result_code == :no_win -> handle_play(win_result_code, input_validation_result, current_player)
+          win_result_code == :wins_game -> handle_play(win_result_code, input_validation_result, current_player)
+          win_result_status == :error -> handle_error(input_validation_result, board, current_player)
+        end
+  end
+
   def play(board \\ initial_board(), prompt \\ :initial_player_prompt, current_player \\ "X") do
     print_board(board)
 
@@ -66,21 +82,11 @@ defmodule TTT do
         handle_error({:error, :board_is_filled}, nil, nil)
 
       true ->
-        input_validation_result = get_player_input(current_player, prompt) |> ProcessInput.handle_input(board, current_player)
+        input_result = get_player_input(current_player, prompt)
+        |> ProcessInput.handle_input(board, current_player)
 
-        {status, _result} = input_validation_result
-        win_result = cond do
-          status == :error -> input_validation_result
-          status == :ok -> CheckForWins.analyze(board)
-        end
-
-        {win_result_status, win_result_code} = win_result
-        cond do
-          win_result_code == :no_win -> handle_play(win_result_code, input_validation_result, current_player)
-          win_result_code == :wins_game -> handle_play(win_result_code, input_validation_result, current_player)
-          win_result_status == :error -> handle_error(input_validation_result, board, current_player)
-        end
-
+        handle_validation_result(input_result, board)
+        |> handle_win_check_result(input_result, current_player, board)
     end
   end
 end
