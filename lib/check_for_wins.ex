489e5bbe2f2632873996588e1f52b8779e7fbe72
serpient <littlegangwolf@gmail.com>
2019-03-26 08:39:34 -0700

    extract check_row fn

--- lib/check_for_wins.ex
+++ lib/check_for_wins.ex
@@ -1,9 +1,11 @@
 defmodule CheckForWins do
+  def check_row(row_data, player_symbol) do
+    Enum.all?(row_data, fn symbol -> symbol == player_symbol end)
+  end
   def analyze(board) do
     wins = Map.values(board)
     |> Enum.chunk_every(3)
-    |> Enum.map(fn (row) -> (Enum.all?(row, fn symbol -> symbol == "X" end)) || (Enum.all?(row, fn symbol -> symbol == "O" end)) end)
-
+    |> Enum.map(fn (row) -> check_row(row, "X") || check_row(row, "O") end)
     cond do
       Enum.member?(wins, true) -> {:ok, :wins_game}
       true -> {:error, :no_win}
