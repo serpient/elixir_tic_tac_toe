121bc615bd22b48bd925de08bc2324c13ced739a
serpient <littlegangwolf@gmail.com>
2019-03-26 07:26:33 -0700

    clean up test file -- remove comments; update descriptions

--- test/ttt_test.exs
+++ test/ttt_test.exs
@@ -1,4 +1,4 @@
-defmodule TTT_Test do
+defmodule TTT_Input do
   use ExUnit.Case
   import ExUnit.CaptureIO
   doctest TTT
@@ -22,12 +22,8 @@ defmodule TTT_Test do
              GameOutput.get_board(initial_board) <> "\n"
   end
 
-  # test "CL outputs starter text for game" do
-  #   assert capture_io(fn -> TTT.get_player_input(:initial_player_prompt) end) ==
-  #            GameOutput.get_message(:initial_player_prompt)
-  # end
 
-  test "After Player X input, board is correctly displayed with new input" do
+  test "After Player X input, board data is updated" do
     initial_board = %{
       1 => " ",
       2 => " ",
@@ -55,7 +51,7 @@ defmodule TTT_Test do
     assert ProcessInput.handle_input("1", initial_board, "X") == {:ok, updated_board}
   end
 
-  test "After Player O input, board is correctly displayed with new input" do
+  test "After Player O input, board data is updated" do
     initial_board = %{
       1 => "X",
       2 => " ",
@@ -83,7 +79,7 @@ defmodule TTT_Test do
     assert ProcessInput.handle_input("4", initial_board, "O") == {:ok, updated_board}
   end
 
-  test "After overlapping input, prompt player for another input." do
+  test "After overlapping input, returns error." do
     initial_board = %{
       1 => "X",
       2 => " ",
@@ -131,7 +127,7 @@ defmodule TTT_Test do
     assert ProcessInput.is_empty(2, initial_board) == true
   end
 
-  test "After no number input, prompt player for another input." do
+  test "After no number input, returns error." do
     initial_board = %{
       1 => "X",
       2 => " ",
@@ -147,7 +143,7 @@ defmodule TTT_Test do
     assert ProcessInput.handle_input("string", initial_board, "X") == {:error, :invalid_input}
   end
 
-  test "After board is filled, don't allow more input" do
+  test "After board is filled, don't allow more input and outputs message." do
     initial_board = %{
       1 => "X",
       2 => "O",
@@ -164,7 +160,7 @@ defmodule TTT_Test do
              GameOutput.get_message(:board_is_filled) <> "\n"
   end
 
-  test "Checks for empty spots in board." do
+  test "Checks for empty spots in board. False is filled." do
     initial_board = %{
       1 => "X",
       2 => "O",
