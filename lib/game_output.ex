5e8ca4ca23dd79bfd1052cc08d4f489047fe43ce
serpient <littlegangwolf@gmail.com>
2019-03-26 11:14:30 -0700

    adjust game output messages

--- lib/game_output.ex
+++ lib/game_output.ex
@@ -6,8 +6,8 @@ defmodule GameOutput do
       invalid_input: ~s(Please enter a number: ),
       board_is_filled: ~s(No more positions to play! The game is a tie.),
       invalid_input_range: ~s{Please enter a number from 1 - 9: },
-      wins_game: ~s(Wins!),
-      nil: ~s( - Select a numbered spot: ),
+      wins_game: ~s(Wins! \n\n),
+      nil: ~s(Select a numbered spot: ),
     }
 
     game_text[key]
@@ -20,7 +20,7 @@ defmodule GameOutput do
     row1 = "1    |2    |3    \n  #{p1}  |  #{p2}  |  #{p3}  \n_____|_____|_____\n"
     row2 = "4    |5    |6    \n  #{p4}  |  #{p5}  |  #{p6}  \n_____|_____|_____\n"
     row3 = "7    |8    |9    \n  #{p7}  |  #{p8}  |  #{p9}  \n     |     |     \n"
-    margin_bottom = "\n\n"
+    margin_bottom = "\n\n\n"
 
     ~s(#{margin_top <> row1 <> row2 <> row3 <> margin_bottom})
   end
