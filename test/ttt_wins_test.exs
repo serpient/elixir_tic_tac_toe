33cae7e0f2225cce1ce70a5a8f140821407c2da5
serpient <littlegangwolf@gmail.com>
2019-03-26 09:28:20 -0700

    add additional test units for vertical wins -- all pass

--- test/ttt_wins_test.exs
+++ test/ttt_wins_test.exs
@@ -79,4 +79,49 @@ defmodule TTT_Check_For_Wins_Test do
     }
     assert CheckForWins.analyze(initial_board) == {:ok, :wins_game}
   end
+
+  test "Player O Wins if there are 3 matching vertical wins" do
+    initial_board = %{
+      1 => "X",
+      2 => "O",
+      3 => " ",
+      4 => "X",
+      5 => "O",
+      6 => " ",
+      7 => " ",
+      8 => "O",
+      9 => " "
+    }
+    assert CheckForWins.analyze(initial_board) == {:ok, :wins_game}
+  end
+
+  test "Wins if there are 3 matching vertical wins in last column" do
+    initial_board = %{
+      1 => " ",
+      2 => " ",
+      3 => "X",
+      4 => " ",
+      5 => " ",
+      6 => "X",
+      7 => " ",
+      8 => " ",
+      9 => "X"
+    }
+    assert CheckForWins.analyze(initial_board) == {:ok, :wins_game}
+  end
+
+  test "Does not win if there are non-matching in a column" do
+    initial_board = %{
+      1 => " ",
+      2 => " ",
+      3 => "X",
+      4 => " ",
+      5 => " ",
+      6 => "O",
+      7 => " ",
+      8 => " ",
+      9 => "X"
+    }
+    assert CheckForWins.analyze(initial_board) == {:error, :no_win}
+  end
 end
