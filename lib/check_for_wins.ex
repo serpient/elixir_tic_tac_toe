6804e1800af94017f2d120a173a5f6d2a4a2d43e
serpient <littlegangwolf@gmail.com>
2019-03-26 09:42:07 -0700

    refactor horz and vertical win fn

--- lib/check_for_wins.ex
+++ lib/check_for_wins.ex
@@ -1,19 +1,33 @@
 defmodule CheckForWins do
-  def check_row(row_data, player_symbol) do
+  def is_win?(row_data, player_symbol) do
     Enum.all?(row_data, fn symbol -> symbol == player_symbol end)
   end
-  def analyze(board) do
-    horizontal_wins = Map.values(board)
-    |> Enum.chunk_every(3)
-    |> Enum.map(fn (row) -> check_row(row, "X") || check_row(row, "O") end)
 
-    vertical_wins =
+  def convert_horizontal_to_row(board) do
+    Map.values(board)
+    |> Enum.chunk_every(3)
+  end
+
+  def convert_vertical_to_row(board) do
     Enum.map(1..3, fn row_idx -> [board[row_idx], board[row_idx + 3], board[row_idx + 6]] end)
-    |> Enum.map(fn (row) -> check_row(row, "X") || check_row(row, "O") end)
+  end
+
+  def check_row(row_data) do
+    wins_list = Enum.map(row_data, fn (row) -> is_win?(row, "X") || is_win?(row, "O") end)
+    cond do
+      Enum.member?(wins_list, true) -> {:ok, :wins_game}
+      true -> {:error, :no_win}
+    end
+  end
+  def analyze(board) do
+    horizontal = convert_horizontal_to_row(board) |> check_row
+
+    vertical = convert_vertical_to_row(board) |> check_row
+
+    wins_type = [horizontal, vertical]
 
     cond do
-      Enum.member?(horizontal_wins, true) -> {:ok, :wins_game}
-      Enum.member?(vertical_wins, true) -> {:ok, :wins_game}
+      Enum.member?(wins_type,{:ok, :wins_game}) -> {:ok, :wins_game}
       true -> {:error, :no_win}
     end
   end
