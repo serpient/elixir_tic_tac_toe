64987d4d58b21df8ed6cf61a958433c0fba38684
serpient <littlegangwolf@gmail.com>
2019-03-26 10:59:22 -0700

    adjust board output to have margin

--- lib/game_output.ex
+++ lib/game_output.ex
@@ -1,12 +1,13 @@
 defmodule GameOutput do
   def get_message(key) do
     game_text = %{
-      initial_player_prompt: ~s( - Select a numbered spot: ),
+      initial_player_prompt: ~s(Select a numbered spot: ),
       duplicate_input: ~s(That spot is taken! Please choose another number: ),
       invalid_input: ~s(Please enter a number: ),
       board_is_filled: ~s(No more positions to play! The game is a tie.),
       invalid_input_range: ~s{Please enter a number from 1 - 9: },
-      wins_game: ~s( Wins!)
+      wins_game: ~s(Wins!),
+      nil: ~s( - Select a numbered spot: ),
     }
 
     game_text[key]
@@ -15,10 +16,12 @@ defmodule GameOutput do
   def get_board(board_data) do
     [p1, p2, p3, p4, p5, p6, p7, p8, p9] = Map.values(board_data)
 
+    margin_top = "\n"
     row1 = "1    |2    |3    \n  #{p1}  |  #{p2}  |  #{p3}  \n_____|_____|_____\n"
     row2 = "4    |5    |6    \n  #{p4}  |  #{p5}  |  #{p6}  \n_____|_____|_____\n"
     row3 = "7    |8    |9    \n  #{p7}  |  #{p8}  |  #{p9}  \n     |     |     \n"
+    margin_bottom = "\n\n"
 
-    ~s(#{row1 <> row2 <> row3})
+    ~s(#{margin_top <> row1 <> row2 <> row3 <> margin_bottom})
   end
 end
