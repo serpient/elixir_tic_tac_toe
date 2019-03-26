73776126c62cf32c5d59b3626437d16f469a2ee7
serpient <littlegangwolf@gmail.com>
2019-03-26 09:26:10 -0700

    passes vertical wins

--- lib/check_for_wins.ex
+++ lib/check_for_wins.ex
@@ -3,11 +3,17 @@ defmodule CheckForWins do
     Enum.all?(row_data, fn symbol -> symbol == player_symbol end)
   end
   def analyze(board) do
-    wins = Map.values(board)
+    horizontal_wins = Map.values(board)
     |> Enum.chunk_every(3)
     |> Enum.map(fn (row) -> check_row(row, "X") || check_row(row, "O") end)
+
+    vertical_wins =
+    Enum.map(1..3, fn row_idx -> [board[row_idx], board[row_idx + 3], board[row_idx + 6]] end)
+    |> Enum.map(fn (row) -> check_row(row, "X") || check_row(row, "O") end)
+
     cond do
-      Enum.member?(wins, true) -> {:ok, :wins_game}
+      Enum.member?(horizontal_wins, true) -> {:ok, :wins_game}
+      Enum.member?(vertical_wins, true) -> {:ok, :wins_game}
       true -> {:error, :no_win}
     end
   end
