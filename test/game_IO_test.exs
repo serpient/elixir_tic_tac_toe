28c6ef15699d8088c90366d5fc020b758b922025
serpient <littlegangwolf@gmail.com>
2019-03-27 10:36:00 -0700

    add get_message test units

--- test/game_IO_test.exs
+++ test/game_IO_test.exs
@@ -27,4 +27,12 @@ defmodule Game_Output_Test do
   test "Toggles player symbols. From O -> X" do
     assert GameIO.get_other_player_symbol("O") == "X"
   end
+
+  test "Gets correct message." do
+    assert GameIO.get_message(:initial_player_prompt) == ~s(Select a numbered spot: )
+  end
+
+  test "Gets default message on nil param" do
+    assert GameIO.get_message(nil) == ~s(Select a numbered spot: )
+  end
 end
