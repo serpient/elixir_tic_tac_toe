4ffb4c16ec69e59ab08e1ceed4ad8bcaa3ea03ba
serpient <littlegangwolf@gmail.com>
2019-03-27 12:35:40 -0700

    add win methods test units

--- test/wins_test.exs
+++ test/wins_test.exs
@@ -180,7 +180,7 @@ defmodule Check_For_Wins_Test do
     assert CheckForWins.analyze(initial_board) == {:error, :no_win, initial_board}
   end
 
-  test "[analyze][has_empty_spaces] Returns False if board is filled." do
+  test "[has_empty_spaces] Returns False if board is filled." do
     initial_board = %{
       1 => "X",
       2 => "O",
@@ -196,7 +196,7 @@ defmodule Check_For_Wins_Test do
     assert CheckForWins.has_empty_spaces?(initial_board) == false
   end
 
-  test "[analyze][has_empty_spaces] Returns True if board has more spaces to play." do
+  test "[has_empty_spaces] Returns True if board has more spaces to play." do
     initial_board = %{
       1 => "X",
       2 => "O",
@@ -212,7 +212,7 @@ defmodule Check_For_Wins_Test do
     assert CheckForWins.has_empty_spaces?(initial_board) == true
   end
 
-  test "[analyze][handle_win_check] Winning board is passed to analyze and results in ok tuple" do
+  test "[handle_win_check] Winning board is passed to analyze and results in ok tuple" do
     initial_board = %{
       1 => "X",
       2 => "X",
@@ -229,7 +229,7 @@ defmodule Check_For_Wins_Test do
              {:ok, :wins_game, initial_board}
   end
 
-  test "[analyze][handle_win_check] Non winning board results in error tuple" do
+  test "[handle_win_check] Non winning board results in error tuple" do
     initial_board = %{
       1 => "X",
       2 => "X",
@@ -246,7 +246,7 @@ defmodule Check_For_Wins_Test do
              {:error, :no_win, initial_board}
   end
 
-  test "[analyze][handle_win_check] Error passed to this function results in error tuple" do
+  test "[handle_win_check] Error passed to this function results in error tuple" do
     initial_board = %{
       1 => "X",
       2 => "X",
@@ -261,5 +261,68 @@ defmodule Check_For_Wins_Test do
 
     assert CheckForWins.handle_win_check({:error, :invalid_input}, initial_board, "X") ==
              {:error, :invalid_input, initial_board}
+  end
+
+  test "[is_win?] Returns true if every symbol in list matches current player symbol" do
+    assert CheckForWins.is_win?(["X", "X", "X"], "X") == true
+  end
+
+  test "[is_win?] Returns false if every symbol in list does not match current player symbol" do
+    assert CheckForWins.is_win?(["X", "X", "X"], "O") == false
+  end
+
+  test "[convert_horizontal_to_row] Converts board correctly" do
+    initial_board = %{
+      1 => "1",
+      2 => "2",
+      3 => "3",
+      4 => "4",
+      5 => "5",
+      6 => "6",
+      7 => "7",
+      8 => "8",
+      9 => "9"
+    }
+    assert CheckForWins.convert_horizontal_to_row(initial_board) == [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
+  end
+
+  test "[convert_vertical_to_row] Converts board correctly" do
+    initial_board = %{
+      1 => "1",
+      2 => "2",
+      3 => "3",
+      4 => "4",
+      5 => "5",
+      6 => "6",
+      7 => "7",
+      8 => "8",
+      9 => "9"
+    }
+    assert CheckForWins.convert_vertical_to_row(initial_board) == [["1", "4", "7"], ["2", "5", "8"], ["3", "6", "9"]]
+  end
+
+  test "[convert_diagonal_to_row] Converts board correctly" do
+    initial_board = %{
+      1 => "1",
+      2 => "2",
+      3 => "3",
+      4 => "4",
+      5 => "5",
+      6 => "6",
+      7 => "7",
+      8 => "8",
+      9 => "9"
+    }
+    assert CheckForWins.convert_diagonal_to_row(initial_board) == [["1", "5", "9"], ["3", "5", "7"]]
+  end
+
+  test "[check_row] Checks each row in data and returns true if there is at least a single winning row" do
+    row = [["X", "O", "X"], ["O", "X", "O"], ["X", "X", "X"]]
+    assert CheckForWins.check_row(row) == true
+  end
+
+  test "[check_row] Checks each row in data and returns false if there is no winning row" do
+    row = [["X", "O", "X"], ["O", "X", "O"], ["X", "O", "X"]]
+    assert CheckForWins.check_row(row) == false
   end
 end
