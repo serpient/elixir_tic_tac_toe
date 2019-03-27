c806b45de2237ea72de71e6a0b88cfdd6783b925
serpient <littlegangwolf@gmail.com>
2019-03-27 10:38:16 -0700

    add error case for board update fn

--- test/update_board_test.exs
+++ test/update_board_test.exs
@@ -2,7 +2,7 @@ defmodule Update_Board_Test do
   use ExUnit.Case
   doctest UpdateBoard
 
-  test "Handle Board update Result" do
+  test "On :ok, Handle Board updates board data" do
     initial_board = %{
       1 => "X",
       2 => "X",
@@ -29,5 +29,20 @@ defmodule Update_Board_Test do
 
     assert UpdateBoard.handle_board_update({:ok, 4}, initial_board, "X") ==
              {:ok, updated_board}
+  end
+
+  test "On :error, Handle Board returns the error" do
+    initial_board = %{
+      1 => "X",
+      2 => "X",
+      3 => "X",
+      4 => " ",
+      5 => " ",
+      6 => " ",
+      7 => " ",
+      8 => " ",
+      9 => " "
+    }
+    assert UpdateBoard.handle_board_update({:error, :invalid_input}, initial_board, "X") == {:error, :invalid_input}
   end
 end
