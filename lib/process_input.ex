d2a44fe26f362847f9a74a20371f386d92e5643c
serpient <littlegangwolf@gmail.com>
2019-03-27 10:11:29 -0700

    refactor to only handle input processing; returns a string

--- lib/process_input.ex
+++ lib/process_input.ex
@@ -1,20 +1,11 @@
 defmodule ProcessInput do
-  def update_board(position_to_update, board_data, player_symbol)
-      when is_integer(position_to_update) do
-    {:ok, Map.replace!(board_data, position_to_update, player_symbol)}
-  end
-
-  def update_board(error, _board_data, _player_symbol) do
-    error
-  end
-
   def transform_to_integer(string) do
     Integer.parse(string)
   end
 
   def process_input(string, _board, _current_player) when is_tuple(string) do
     string
-    |> Tuple.to_list
+    |> Tuple.to_list()
     |> Enum.fetch!(0)
   end
 
@@ -28,7 +19,7 @@ defmodule ProcessInput do
 
   def validate_input(result, board, _current_player) when is_integer(result) do
     cond do
-      is_empty(result, board) == true -> result
+      is_empty(result, board) == true -> {:ok, result}
       result > 9 || result < 1 -> {:error, :invalid_input_range}
       true -> {:error, :duplicate_input}
     end
@@ -38,25 +29,10 @@ defmodule ProcessInput do
     error
   end
 
-  def handle_validation_result(input_validation_result) do
-    {status, _results} = input_validation_result
-
-    case status do
-      :error -> input_validation_result
-      _ -> input_validation_result
-    end
-  end
-
   def handle_input(input, board, current_player) do
     input
     |> transform_to_integer
     |> process_input(board, current_player)
     |> validate_input(board, current_player)
-    |> update_board(board, current_player)
-    |> handle_validation_result
   end
 end
-
-# feels weird that im passing errors down through the entire chain
-# before it is resolved.
-# i want to break out of chain when I see error
