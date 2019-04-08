04de417fbd2ef0635e39455502a44cc589fa5cf8
serpient <littlegangwolf@gmail.com>
2019-04-08 12:05:31 -0700

    refactor board printer; isolate cell printers

--- lib/board.ex
+++ lib/board.ex
@@ -42,45 +42,40 @@ defmodule Board do
     |> Enum.reduce(%{}, fn (num, acc) -> Map.put(acc, num, " ") end)
   end
 
+  def get_cell_line_to_print(cell_level, at_end_of_row?, value) do
+    line_to_print = case cell_level do
+      "top" -> "#{value}    "
+      "middle" -> "  #{value}  "
+      "bottom" -> "_____"
+      "bottom_of_last_row" -> "     "
+    end
+
+    if at_end_of_row?, do: line_to_print, else: line_to_print <> "|"
+  end
+
+  def print_cell_line(cell_level, num_of_columns, row_idx, board_data) do
+    Enum.reduce(1..num_of_columns, "", fn (column_idx, acc) ->
+      curr_column_number = row_idx * num_of_columns + (column_idx)
+      cell_value = if cell_level == "middle", do: Map.get(board_data, curr_column_number), else: curr_column_number
+      acc <> get_cell_line_to_print(cell_level, column_idx == num_of_columns, cell_value)
+    end ) <>  "\n"
+  end
+
+  def print_cell_row(num_of_columns, num_of_rows, row_idx, board_data) do
+    top_line = print_cell_line("top", num_of_columns, row_idx, board_data)
+    middle_line = print_cell_line("middle", num_of_columns, row_idx, board_data)
+    last_line = case num_of_rows - 1 == row_idx do
+      true -> print_cell_line("bottom_of_last_row", num_of_columns, row_idx, board_data)
+      false -> print_cell_line("bottom", num_of_columns, row_idx, board_data)
+    end
+    top_line <> middle_line <> last_line
+  end
   def generate_board_for_print(num_of_rows, num_of_columns, board_data) do
     new_line = "\n"
     margin_bottom = "\n\n\n"
 
     Enum.reduce(0..num_of_rows - 1, new_line, fn (row_idx, acc) ->
-      acc <> Enum.reduce(1..num_of_columns, "", fn (column_idx, acc) ->
-        curr_column_number = row_idx * num_of_columns + (column_idx)
-        print = cond do
-          (column_idx == num_of_columns) == false -> "#{curr_column_number}    |"
-          (column_idx == num_of_columns) == true -> "#{curr_column_number}    "
-        end
-        acc <> print
-      end )
-      <> new_line
-      <> Enum.reduce(1..num_of_columns, "", fn (column_idx, acc) ->
-        curr_column_number = row_idx * num_of_columns + (column_idx)
-        curr_column_value = Map.get(board_data, curr_column_number)
-        print = cond do
-          (column_idx == num_of_columns) == false -> "  #{curr_column_value}  |"
-          (column_idx == num_of_columns) == true -> "  #{curr_column_value}  "
-        end
-        acc <> print
-      end )
-      <> new_line
-      <> Enum.reduce(1..num_of_columns, "", fn (column_idx, acc) ->
-        print = if (num_of_rows - 1 == row_idx) do
-          cond do
-            (column_idx == num_of_columns) == false -> "     |"
-            (column_idx == num_of_columns) == true -> "     "
-          end
-        else
-          cond do
-            (column_idx == num_of_columns) == false -> "_____|"
-            (column_idx == num_of_columns) == true -> "_____"
-          end
-        end
-        acc <> print
-      end ) <> new_line
-    end
-    ) <> margin_bottom
+      acc <> print_cell_row(num_of_columns, num_of_rows, row_idx, board_data)
+    end) <> margin_bottom
   end
 end
