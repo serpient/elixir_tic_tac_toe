c1aa193bb94c6b844b8835ac15198208ca86659b
serpient <littlegangwolf@gmail.com>
2019-04-09 11:32:20 -0700

    format board file

--- lib/board.ex
+++ lib/board.ex
@@ -45,13 +45,14 @@ defmodule Board do
     %Board{board_data: board, num_of_columns: columns, num_of_rows: rows, max_spaces: max_spaces} =
       board_spec
 
-      1..rows
-      |> Enum.map(fn row_idx -> take_board_value_by_every_num(row_idx..max_spaces, columns, columns, board) end)
+    1..rows
+    |> Enum.map(fn row_idx ->
+      take_board_value_by_every_num(row_idx..max_spaces, columns, columns, board)
+    end)
   end
 
   def convert_diagonal_to_row(board_spec) do
-    %Board{board_data: board, num_of_columns: columns, max_spaces: max_spaces} =
-      board_spec
+    %Board{board_data: board, num_of_columns: columns, max_spaces: max_spaces} = board_spec
 
     [1, columns]
     |> Enum.map(fn corner_idx ->
@@ -97,12 +98,19 @@ defmodule Board do
     end)
   end
 
-  def compile_list_of_cell_parts(num_of_cells_to_create, num_of_columns, num_of_rows, row_idx, board_data) do
+  def compile_list_of_cell_parts(
+        num_of_cells_to_create,
+        num_of_columns,
+        num_of_rows,
+        row_idx,
+        board_data
+      ) do
     num_of_cells_to_create
     |> Enum.map(fn column_idx ->
       curr_column_number = row_idx * num_of_columns + column_idx
       at_end_of_cell? = column_idx == num_of_columns
       bottom = if num_of_rows - 1 == row_idx, do: "bottom_of_last_row", else: "bottom"
+
       [
         get_cell_part("top", at_end_of_cell?, curr_column_number),
         get_cell_part("middle", at_end_of_cell?, board_data[curr_column_number]),
