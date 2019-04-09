76909cdc1368d9e06ee97a341f87bfbd8c755c16
serpient <littlegangwolf@gmail.com>
2019-04-09 14:57:21 -0700

    refactor -- rename; move around pipe fns in board

--- lib/board.ex
+++ lib/board.ex
@@ -97,44 +97,40 @@ defmodule Board do
     end
   end
 
+  def list_of_a_single_cells_parts(
+    column_idx,
+    num_of_columns,
+    num_of_rows,
+    row_idx,
+    board_data
+  ) do
+  curr_column_number = row_idx * num_of_columns + column_idx
+  at_end_of_cell? = column_idx == num_of_columns
+  bottom = if num_of_rows - 1 == row_idx, do: "bottom_of_last_row", else: "bottom"
+
+  [
+    get_cell_part("top", at_end_of_cell?, curr_column_number),
+    get_cell_part("middle", at_end_of_cell?, board_data[curr_column_number]),
+    get_cell_part(bottom, at_end_of_cell?, nil)
+  ]
+end
+
   def compile_row_of_cells(num_of_columns, num_of_rows, row_idx, board_data) do
     1..num_of_columns
-    |> Enum.map(fn column_idx -> compile_list_of_single_cell_parts(column_idx, num_of_columns, num_of_rows, row_idx, board_data) end)
+    |> Enum.map(fn column_idx -> list_of_a_single_cells_parts(column_idx, num_of_columns, num_of_rows, row_idx, board_data) end)
     |> Enum.zip()
     |> Enum.map_join(fn value ->
       Tuple.append(value, "\n") |> Tuple.to_list()
     end)
   end
 
-  def compile_list_of_single_cell_parts(
-        column_idx,
-        num_of_columns,
-        num_of_rows,
-        row_idx,
-        board_data
-      ) do
-      curr_column_number = row_idx * num_of_columns + column_idx
-      at_end_of_cell? = column_idx == num_of_columns
-      bottom = if num_of_rows - 1 == row_idx, do: "bottom_of_last_row", else: "bottom"
-
-      [
-        get_cell_part("top", at_end_of_cell?, curr_column_number),
-        get_cell_part("middle", at_end_of_cell?, board_data[curr_column_number]),
-        get_cell_part(bottom, at_end_of_cell?, nil)
-      ]
-  end
-
   def generate_board_for_print(board_spec) do
-    new_line = "\n"
-    margin_bottom = "\n\n\n"
-
-    %Board{board_data: board_data, num_of_columns: num_of_columns, num_of_rows: num_of_rows} =
-      board_spec
+    %Board{board_data: board_data, num_of_columns: num_of_columns, num_of_rows: num_of_rows} = board_spec
 
     0..(num_of_rows - 1)
     |> Enum.map(fn row_idx -> compile_row_of_cells(num_of_columns, num_of_rows, row_idx, board_data) end)
-    |> List.insert_at(0, new_line)
-    |> List.insert_at(-1, margin_bottom)
+    |> List.insert_at(0, "\n")
+    |> List.insert_at(-1, "\n\n\n")
     |> Enum.join
   end
 
